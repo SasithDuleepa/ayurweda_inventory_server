68b2bd7657e3a457172cd6c216ac63424fcc9950
SasithDuleepa <duleepajayathissa@gmail.com>
2024-02-13 18:04:01 +0530

    item qty

--- config/Database.js
+++ config/Database.js
@@ -5,9 +5,23 @@ class Database {
     constructor(){
         this.connection = mysql.createConnection({
             host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,});
+            database: process.env.DB_NAME
+        });
+
+        // this.connection = mysql.createConnection({
+        //     host: process.env.DB_HOST,
+        //     user: process.env.DB_USER,
+        //     password: process.env.DB_PASSWORD,
+        //     database:process.env.DB_NAME,
+        //     port: process.env.DB_PORT,  
+        //     ssl  : {
+        //         ca : fs.readFileSync('<path to CA cert file>'),
+        //         rejectUnauthorized: true
+        //         }
+        // });
     }
 
     connect(){
--- routes/InventoryAll.js
+++ routes/InventoryAll.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const GetAllAccToName = require('../controllers/inventory all/get_all_acc_to_name');
+const GetQtyAccToItemId = require('../controllers/inventory all/get_qty_acc_to_item_id');
 
 const router = express.Router();
 
 router.get('/:name', GetAllAccToName);  
+router.get('/qty/:id', GetQtyAccToItemId);
 
 module.exports = router;
