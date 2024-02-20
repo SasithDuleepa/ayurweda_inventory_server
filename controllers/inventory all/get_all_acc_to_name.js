618d3b801df89ebea7b8e290da16bdcc0cc43d70
SasithDuleepa <duleepajayathissa@gmail.com>
2024-02-20 17:55:20 +0530

    edited

--- controllers/inventory all/get_all_acc_to_name.js
+++ controllers/inventory all/get_all_acc_to_name.js
@@ -5,20 +5,20 @@ const GetAllAccToName = (req,res) =>{
     console.log(name)
     if(name){
         const sql =`
-        SELECT _id AS identifier , non_raw_item_id AS item_id, non_raw_item_name AS item_name, 'inventory_store_non_raw' AS table_name
+        SELECT _id AS identifier ,non_raw_inventory_item_id AS Inventory_item_id, non_raw_item_id AS item_id, non_raw_item_name AS item_name, 'inventory_store_non_raw' AS table_name
         FROM inventory_store_non_raw
         WHERE non_raw_item_name LIKE '%${name}%'
 
         UNION
 
-        SELECT 'inventory_store_products.product_id', inventory_store_products.product_id,product.product_name, 'inventory_store_products'
+        SELECT 'inventory_store_products.product_id',inventory_store_products.inventory_product_id, inventory_store_products.product_id,product.product_name, 'inventory_store_products'
         FROM product
         INNER JOIN inventory_store_products ON product.product_id = inventory_store_products.product_id
         WHERE product.product_name LIKE '%${name}%'
 
         UNION
         
-        SELECT 'inventory_store_raw_items._id', inventory_store_raw_items.raw_item_id,raw_item.raw_item_name, 'inventory_store_raw_items'
+        SELECT 'inventory_store_raw_items._id',inventory_store_raw_items.raw_item_inventory_id, inventory_store_raw_items.raw_item_id,raw_item.raw_item_name, 'inventory_store_raw_items'
         FROM raw_item
         INNER JOIN inventory_store_raw_items ON raw_item.raw_item_id = inventory_store_raw_items.raw_item_id
         WHERE raw_item_name LIKE '%${name}%';`
